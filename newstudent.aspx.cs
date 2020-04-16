5111a859e7447a448d43019fdb09ab28de2cbf00
sohailakhaled <61541946+sohailakhaled@users.noreply.github.com>
2020-04-16 14:42:56 +0200

    Add files via upload

--- newstudent.aspx.cs
+++ newstudent.aspx.cs
@@ -5,14 +5,49 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class newstudent : System.Web.UI.Page
+public partial class newstudent : 
+   
+        
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString="Data Source=(LocalDB)\\v11.0;AttachDbFilename=|data directory| studentdo.mdf;Integrated Security=True";
+      
+        
+        string strInsert = "INSERT INTO Table "
+                             + " VALUES('" + txtusername.Text + "', '"
+                             + txtfirstname.Text + "', '"
+                             + txtlastname.Text + "', '"
+                             + ddlcountry.SelectedValue + "', '"       
+                             + ddlfaclty.SelectedValue + "', '"     
+                             + rblgender.SelectedValue + "', '"
+                             + txtemail.Text + "', '"
+                            + txtaddress.Text + "', '"
+                            + txtPassword.Text + "','" 
+                             + txtconfirPassword.Text + "')";
+        
+        // Create SQL Command
+        SqlCommand cmdInsert = new SqlCommand(strInsert, conn);
+
+        conn.Open();
+
+        cmdInsert.ExecuteNonQuery();
+
+        conn.Close();
+
+
+
+        lblMsg.Text = "Welcome " + txtfirstname.Text +  "Your Account Has Been Successfully Created!!";
+          
+        
+    }
+
+    
+</script>
+    protected void Button1_Click(object sender, EventArgs e)
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
 
-    }
-    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-
-    }
+}
+}
+   
 }
